74d7ddc6909e22f0fa7f2432b117336d33f0c749
Tim Kunz <tim.kunz@edu.tbz.ch>
2024-07-03 08:13:12 +0200

    Projektdateien hinzufügen.

---
+++ Ueb11a_StrassenverkehrsamtTest/UnitTestPerson.cs
@@ -0,0 +1,16 @@
+using Ueb11a_Strassenverkehrsamt;
+
+namespace Ueb11a_StrassenverkehrsamtTest
+{
+    [TestClass]
+    public class UnitTestPerson
+    {
+        [TestMethod]
+        public void TestMethod_Konstruktor()
+        {
+            Person p = new Person();
+
+            Assert.IsTrue(true);
+        }
+    }
+}
